bf26ea5a32d0ba9c82241b27b566ebe9efa421a6
Omkar Parekh <omkarparekhneosoft@gmail.com>
2023-11-21 17:19:50 +0530

    second

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express")
 const app = express();
+const path = require('path');
 
 const port = 8080;
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
 // app.use(express.static("public"))
 app.get("/.well-known/assetlinks.json", (req, res) => {
     res.send([
