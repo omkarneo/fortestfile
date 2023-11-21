e37be1e2e0875a4c754198effb3327c53dcbcc0b
Omkar Parekh <omkarparekhneosoft@gmail.com>
2023-11-21 17:07:38 +0530

    second

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ const express = require("express")
 const app = express();
 
 const port = 8080;
-app.use(express.static("public"))
-app.get("/", (req, res) => {
-    res.send("saasd")
+// app.use(express.static("public"))
+app.get("/.well-known/assetslinks.json", (req, res) => {
+    res.send([
+        {
+            "relation": ["delegate_permission/common.handle_all_urls"],
+            "target": {
+                "namespace": "unilinksdemo",
+                "package_name": "com.example.unilinksdemo",
+                "sha256_cert_fingerprints": [
+                    "3C:13:7D:68:EA:B6:6A:08:46:E7:0A:16:16:92:A5:0B:F6:B7:0A:65:8E:A5:64:DC:0D:E5:96:3B:E4:0A:EF:54"
+                ]
+            }
+        }
+    ])
 })
 
 app.listen(port, (req, res) => {
